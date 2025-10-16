aa83d0d2ab77154653f9806604c28c41b5f80fe8
faizsyafiqn <faizsyafiqn@gmail.com>
2025-10-16 16:37:08 +0700

    gaskan pabw

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    public function edit()
+    {
+        $user = [
+            'nama' => 'Faiz Syafiq Nabily',
+            'email' => 'faiz@gmail.com',
+            'telepon' => '081234567890',
+            'tanggal_lahir' => '2005-04-04',
+            'jenis_kelamin' => 'Laki-laki',
+            'alamat' => 'Jl. Merdeka No. 45, Bandung',
+            'foto' => 'faiz.jpg'
+        ];
+
+        return view('editProfil', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+       
+        return redirect()->back()->with('success', 'Profil berhasil diperbarui!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/edit-profil', [ProfilController::class, 'edit'])->name('profil.edit');
+Route::post('/edit-profil', [ProfilController::class, 'update'])->name('profil.update');
