fafe1428ae808bafb15797bc999ff158ef0411bb
faizsyafiqn <faizsyafiqn@gmail.com>
2025-10-19 15:32:24 +0700

    pm

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,18 +8,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-
         $destinasi = [
-            ['nama' => 'Pantai Pangandaran', 'lokasi' => 'Ciamis', 'rating' => 4.7],
-            ['nama' => 'Situ Lengkong Panjalu', 'lokasi' => 'Panjalu', 'rating' => 4.5],
-            ['nama' => 'Green Canyon', 'lokasi' => 'Cijulang', 'rating' => 4.8],
-            ['nama' => 'Curug Tujuh Cibolang', 'lokasi' => 'Panjalu', 'rating' => 4.6],
-            ['nama' => 'Pantai Batu Hiu', 'lokasi' => 'Parigi', 'rating' => 4.4],
-            ['nama' => 'Pantai Karapyak', 'lokasi' => 'Kalipucang', 'rating' => 4.3],
-            ['nama' => 'Bukit Panenjoan', 'lokasi' => 'Cidolog', 'rating' => 4.5],
-            ['nama' => 'Kampung Adat Kuta', 'lokasi' => 'Tambaksari', 'rating' => 4.6],
+            ['nama' => 'Bali', 'lokasi' => 'Bali, Indonesia', 'rating' => 4.9],
+            ['nama' => 'Yogyakarta', 'lokasi' => 'Yogyakarta, Indonesia', 'rating' => 4.8],
+            ['nama' => 'Lombok', 'lokasi' => 'Nusa Tenggara Barat, Indonesia', 'rating' => 4.7],
         ];
 
-        return view('home', ['destinasi' => $destinasi]);
+        return view('home', compact('destinasi'));
     }
 }
