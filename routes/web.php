767e2b8bbf84d2a4e0f9b9880b9f80ff45de42ca
ikaaacan <riskadeabaki0108@example.com>
2025-10-19 20:28:48 +0700

    bismilah

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use App\Http\Controllers\PropertiController;
 use App\Http\Controllers\LokasiController;
 
 
-
-Route::get('/', [LoginController::class, 'showLoginForm'])->name('home');
-Route::get('/edit-profil', [ProfilController::class, 'edit'])->name('profil.edit');
-Route::post('/edit-profil', [ProfilController::class, 'update'])->name('profil.update');    
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/editProfil', [editProfilController::class, 'index'])->name('editProfil');
 Route::post('/updateProfil', [editProfilController::class, 'update'])->name('updateProfil');
@@ -28,7 +24,7 @@ Route::post('/add-property-ptw', [AddPropertyPTWController::class, 'store'])->na
 Route::get('/edit-property-ptw/{id}', [EditPropertyPTWController::class, 'edit'])->name('edit.property.ptw');
 Route::post('/edit-property-ptw/{id}', [EditPropertyPTWController::class, 'update'])->name('update.property.ptw');
 Route::delete('/delete-property-ptw/{id}', [EditPropertyPTWController::class, 'destroy'])->name('delete.property.ptw');
-Route::get('/lokasi/{id}', [PropertiController::class, 'show']);
+Route::get('/lokasi/{id}', [LokasiController::class, 'show']);
 
 Route::get('/homeWisatawan', function () {
     return redirect('/home');
