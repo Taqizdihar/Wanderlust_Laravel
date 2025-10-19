8ede04712da22bc257ac97fbe8df68231f6be3f9
Taqizdihar <m.taqizdihar@gmail.com>
2025-10-19 20:19:44 +0700

    Bismillah Hari Jumat Ya Allah

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
 </head>
 <body>
     <div class="login-container">
