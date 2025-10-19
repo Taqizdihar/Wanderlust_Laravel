28e694c9bc26c3779c651028a9aab4ba9f4b8ff7
Taqizdihar <m.taqizdihar@gmail.com>
2025-10-19 09:27:41 +0700

    Bismillah Hari Jumat

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LoginController extends Controller {
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('login');
     }
 
     public function login(Request $request)
