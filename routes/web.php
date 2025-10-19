d3127fabf52428d594d2d76f3deaa390b993e2b8
Taqizdihar <m.taqizdihar@gmail.com>
2025-10-19 09:25:35 +0700

    Bismillah Hari Jumat

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\editProfilController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('home');
 Route::get('/edit-profil', [ProfilController::class, 'edit'])->name('profil.edit');
 Route::post('/edit-profil', [ProfilController::class, 'update'])->name('profil.update');    
 Route::get('/home', [HomeController::class, 'index'])->name('home');
