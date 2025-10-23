256cf429958107b4eb74a4132c6cb10422973068
faizsyafiqn <faizsyafiqn@gmail.com>
2025-10-24 01:18:34 +0700

    semangat pabw nanti ges

--- resources/views/pencarian.blade.php
+++ resources/views/pencarian.blade.php
@@ -3,37 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Hasil Pencarian</title>
-    <link rel="stylesheet" href="{{ asset('css/Pencarian.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Home.css') }}">
 </head>
 <body>
     <main class="main-content">
         <div class="section-title">
-            <h2>Result for {{ $kataKunci }}</h2>
+            <h2>Hasil Pencarian: "{{ $kataKunci }}"</h2>
+            <p class="subtitle">Menampilkan destinasi wisata berdasarkan pencarianmu</p>
+        </div>
 
-            <div class="filter">
-                <label>Category</label>
-                <select>
-                    <option selected>Most Relevant</option>
-                </select>
+        @if(count($hasilPencarian) > 0)
+            <div class="card-gallery">
+                @foreach ($hasilPencarian as $hasil)
+                    <div class="cards-destination">
+                        <div class="card-images" style="background-image: url('{{ asset('/images/Images/' . $hasilPencarian['link_foto']) }}');">
+                            <h4>{{ $hasil['nama_lokasi'] }}</h4>
+                        </div>
+                        <div class="destination-content">
+                            <p>{{ $hasil['sumir'] }}</p>
+                            <div class="stars">
+                                ★★★★☆
+                            </div>
+                            <a class="card-button" href="{{ url('detailDestinasiWisata?tempatwisata_id=' . $hasil['tempatwisata_id']) }}">Lihat Detail</a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        </div>
-
-        <div class="card-gallery">
-            @forelse ($hasilPencarian as $hasil)
-                <div class="cards-destination">
-                    <div class="card-images" style="background-image: url('{{ asset('pemilikWisata/foto/' . $hasil['link_foto']) }}');">
-                        <h4>{{ $hasil['nama_lokasi'] }}</h4>
-                    </div>
-                    <div class="destination-content">
-                        <p>{{ $hasil['sumir'] }}</p>
-                        <div class="stars"></div>
-                        <a class="card-button" href="{{ url('detailDestinasiWisata?tempatwisata_id=' . $hasil['tempatwisata_id']) }}">Check</a>
-                    </div>
-                </div>
-            @empty
-                <p>Tidak ada hasil ditemukan untuk "{{ $kataKunci }}"</p>
-            @endforelse
-        </div>
+        @else
+            <div class="no-results">
+                <p>Tidak ada hasil ditemukan untuk "<strong>{{ $kataKunci }}</strong>".</p>
+                <a href="{{ url('/') }}" class="back-home">Kembali ke Beranda</a>
+            </div>
+        @endif
     </main>
 </body>
 </html>
