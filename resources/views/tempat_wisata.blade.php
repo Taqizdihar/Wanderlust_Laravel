abe44a78defeedeeb91afa240545573027e939e7
ikaaacan <riskadeabaki0108@example.com>
2025-10-24 16:50:10 +0700

    p

--- resources/views/tempat_wisata.blade.php
+++ resources/views/tempat_wisata.blade.php
@@ -8,7 +8,7 @@
   <table>
     <thead>
       <tr>
-        <th>No</th>
+         <th>No</th>
         <th>Nama Tempat</th>
         <th>Lokasi</th>
         <th>Harga Tiket</th>
@@ -19,22 +19,23 @@
     <tbody>
       @foreach ($data_wisata as $index => $wisata)
         <tr>
-          <td>{{ $index + 1 }}</td>
+         <td>{{ $index + 1 }}</td>
           <td>{{ $wisata['nama'] }}</td>
           <td>{{ $wisata['lokasi'] }}</td>
-          <td>Rp{{ number_format($wisata['harga'], 0, ',', '.') }}</td>
+
+         <td>Rp{{ number_format($wisata['harga'], 0, ',', '.') }}</td>
           <td>
                 <span class="badge {{ $wisata['status'] == 'Selesai' ? 'success' : 'warning' }}">
                     {{ $wisata['status'] }}
                 </span>
             </td>
-          <td>
-                        <a href="{{ route('verifikasi.detail', $wisata['id']) }}" class="btn" style="background: #17a2b8;">
+         <td>
+             <a href="{{ route('verifikasi.detail', $wisata['id']) }}" class="btn" style="background: #17a2b8;">
                 Verifikasi
             </a>
-          </td>
-        </tr>
-      @endforeach
+         </td>
+         </tr>
+         @endforeach
     </tbody>
   </table>
 @endsection
