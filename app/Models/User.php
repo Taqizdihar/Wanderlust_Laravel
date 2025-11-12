3e3c74991702c54f1ddee3090227a31c3afdb7c4
Taqizdihar <m.taqizdihar@gmail.com>
2025-11-13 00:55:55 +0700

    Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
-{
-    //
+class User extends Authenticatable {
+
+    use HasFactory, Notifiable;
+    protected $primaryKey = 'id_user';
+    protected $fillable = ['nama','email','phone_number','password',];
+    protected $hidden = ['password','remember_token',];
+
+    public function administrator() {
+        return $this->hasOne(Administrator::class, 'id_user', 'id_user');
+    }
+
+    public function wisatawan() {
+        return $this->hasOne(Wisatawan::class, 'id_user', 'id_user');
+    }
+
+    public function pemilikTempatWisata() {
+        return $this->hasOne(PemilikTempatWisata::class, 'id_user', 'id_user');
+    }
 }
